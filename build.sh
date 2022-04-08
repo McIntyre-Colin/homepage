e249f02f5a1cbff87dfd211fff089219a9f860b2
McIntyre-Colin <colinhmcintyre@gmail.com>
2022-04-08 15:20:29 -0400

    Adding comments

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
+#Removing Existing files
+
 rm ./docs/index.html
 rm ./docs/projects.html
 rm ./docs/resume.html
 
+#creating new files
 cat ./templates/header.html ./content/index.html ./templates/footer.html > ./docs/index.html
 cat ./templates/header.html ./content/projects.html ./templates/footer.html > ./docs/projects.html
 cat ./templates/header.html ./content/resume.html ./templates/footer.html > ./docs/resume.html

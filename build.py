c2733240fa4f04ddd4fd15f0eecbd459d4d9637a
McIntyre-Colin <colinhmcintyre@gmail.com>
2022-04-08 15:09:33 -0400

    Adding python build script

---
+++ build.py
@@ -0,0 +1,19 @@
+#Constant across pages
+
+header = open('./templates/header.html').read()
+footer = open('./templates/footer.html').read()
+
+#Page content
+index = open('./content/index.html').read()
+projects = open('./content/projects.html').read()
+resume = open('./content/resume.html').read()
+
+#Writing to file
+index = header + index + footer
+projects = header + projects + footer
+resume = header + resume + footer
+
+
+open('./docs/index.html', 'w+').write(index)
+open('./docs/projects.html', 'w+').write(projects)
+open('./docs/resume.html', 'w+').write(resume)
